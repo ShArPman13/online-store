e35eb6a1515a72d985600e0d6eda27c1880fb63d
Alex Sharp <105282542+ShArPman13@users.noreply.github.com>
2022-12-15 21:17:28 +0100

    feat: goods page draft (#6)

---
+++ src/core/components/goodCardSmall.ts
@@ -0,0 +1,69 @@
+import { IData } from '../../types/dataJSON';
+
+export class goodCardSmall {
+  container = document.createElement('div');
+  images;
+  price;
+  brand;
+  title;
+
+  constructor(card: IData) {
+    this.images = card.images;
+    this.price = card.price;
+    this.brand = card.brand;
+    this.title = card.title;
+  }
+
+  render() {
+    this.container.className = 'card-small-container';
+
+    const imageContainer = document.createElement('div');
+    imageContainer.className = 'card__image-container';
+
+    const image = document.createElement('img');
+    image.className = 'image-container__image';
+    image.src = this.images[0];
+    image.alt = this.title;
+
+    imageContainer.append(image);
+
+    const textContainer = document.createElement('div');
+    textContainer.className = 'bottom-container__text-container';
+
+    const price = document.createElement('div');
+    price.className = 'text-container__price';
+    price.innerText = `${this.price}$`;
+
+    const brand = document.createElement('div');
+    brand.className = 'text-container__brand';
+    brand.innerText = this.brand;
+
+    const title = document.createElement('div');
+    title.className = 'text-container__title';
+    title.innerText = this.title;
+
+    textContainer.append(price, brand, title);
+
+    const bottomContainer = document.createElement('div');
+    bottomContainer.className = 'card__bottom-container';
+
+    bottomContainer.append(textContainer, this.addToBasket());
+
+    this.container.append(imageContainer, bottomContainer);
+    return this.container;
+  }
+
+  addToBasket() {
+    const basketImg = document.createElement('img');
+    basketImg.className = 'bottom-container__basketImg';
+    basketImg.src = '../assets/svg/basket_add.svg';
+
+    basketImg.addEventListener('click', () => {
+      basketImg.classList.toggle('in-basket');
+      if (basketImg.classList.contains('in-basket')) {
+        basketImg.src = '../assets/svg/basket_added.svg';
+      } else basketImg.src = '../assets/svg/basket_add.svg';
+    });
+    return basketImg;
+  }
+}
--- src/pages/home/HomePage.ts
+++ src/pages/home/HomePage.ts
@@ -10,11 +10,10 @@ export class HomePage extends Page {
   }
 
   render() {
-    const title = this.createHeaderTitle(HomePage.textObject.mainTitle);
     const shopButton = document.createElement('button');
     shopButton.className = 'main-container__button';
     shopButton.innerText = 'Go Shopping';
-    this.container.append(title, shopButton);
+    this.container.append(shopButton);
     return this.container;
   }
 }
